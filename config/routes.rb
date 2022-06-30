73013606a47b3730a6e284d1042367bb492c6d22
hudaswanditya <huda.swanditya@gmail.com>
2022-06-30 19:56:32 +0200

    Install devise with rails 7 turbo workaround

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
+  devise_for :users
+
   mount Spina::Engine => '/'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root to: "home#index"
 end
